45a2abc3856e9852665689897a8dbee9aaadd2b3
Jacob Buck <buck.jacob@gmail.com>
2020-08-29 23:30:11 +0000

    Update imports to reflect React documentation.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import { Children, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import React, { useRef } from 'react';
+import ReactDOM from 'react-dom';
 import useIsomorphicLayoutEffect from 'use-isomorphic-layout-effect';
 
 const DocumentPortal = (props) => {
-  const child = Children.only(props.children);
+  const child = React.Children.only(props.children);
   const nodeRef = useRef(document.createElement('div'));
 
   useIsomorphicLayoutEffect(() => {
@@ -13,7 +13,7 @@ const DocumentPortal = (props) => {
     return () => document.body.removeChild(node);
   }, []);
 
-  return createPortal(child, nodeRef.current);
+  return ReactDOM.createPortal(child, nodeRef.current);
 };
 
 DocumentPortal.propTypes = {
