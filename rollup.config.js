c88a965c3c2f48a39c2f6fa7f784457df4212e39
Jacob Buck <buck.jacob@gmail.com>
2020-08-30 02:02:01 +0000

    Add and configure babel and rollup. Add build script.

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: 'lib/index.cjs.js', format: 'cjs' },
+    { file: 'lib/index.esm.js', format: 'esm' },
+  ],
+  external: [
+    'prop-types',
+    'react',
+    'react-dom',
+    'use-isomorphic-layout-effect',
+  ],
+  plugins: [babel({ babelHelpers: 'bundled' })],
+};
