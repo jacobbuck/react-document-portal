329603f0ac5071dfc9caa0559f391cbdf66477b2
Jacob Buck <buck.jacob@gmail.com>
2020-09-30 07:02:54 +0000

    Add updateRef.test.js

---
+++ src/__tests__/updateRef.test.js
@@ -0,0 +1,25 @@
+import { createRef } from 'react';
+import updateRef from '../updateRef';
+
+test('handles function refs', () => {
+  const ref = jest.fn();
+  const value = { foo: 'bar' };
+  updateRef(ref, value);
+  expect(ref).toHaveBeenCalledWith(value);
+});
+
+test('handles object refs', () => {
+  const ref = createRef();
+  const value = { foo: 'bar' };
+  updateRef(ref, value);
+  expect(ref).toMatchObject({ current: value });
+});
+
+test('doesn‘t throw when passed an invalid ref', () => {
+  expect(() => {
+    const value = { foo: 'bar' };
+    updateRef(null, value);
+    updateRef(true, value);
+    updateRef('ref', value);
+  }).not.toThrow();
+});
