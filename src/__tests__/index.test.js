55682627f4132cdc12a3fa702abdba7b51e11701
Jacob Buck <buck.jacob@gmail.com>
2020-10-17 10:04:25 +0000

    Update index.test.js

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -2,17 +2,14 @@ import { render } from '@testing-library/react';
 import * as React from 'react';
 import DocumentPortal from '..';
 
-const TestComponent = () => (
-  <main data-testid="main">
-    <p>Example application</p>
-    <DocumentPortal>
-      <dialog data-testid="dialog">Hello!</dialog>
-    </DocumentPortal>
-  </main>
-);
-
 test('renders child inside portal', () => {
-  const { getByTestId } = render(<TestComponent />);
+  const { getByTestId } = render(
+    <main data-testid="main">
+      <DocumentPortal>
+        <dialog data-testid="dialog">Hello!</dialog>
+      </DocumentPortal>
+    </main>
+  );
   const dialogElement = getByTestId('dialog');
   expect(getByTestId('main')).not.toContainElement(dialogElement);
   expect(dialogElement).toBeInTheDocument();
@@ -20,17 +17,38 @@ test('renders child inside portal', () => {
 });
 
 test('appends element to document body element on mount', () => {
-  const { baseElement, getByTestId } = render(<TestComponent />);
+  const { baseElement, getByTestId } = render(
+    <DocumentPortal>
+      <dialog data-testid="dialog">Hello!</dialog>
+    </DocumentPortal>
+  );
   expect(baseElement.lastChild).toBe(getByTestId('dialog').parentNode);
 });
 
 test('removes element from document body element unmount', () => {
-  const { getByTestId, unmount } = render(<TestComponent />);
-  const parentElement = getByTestId('main');
+  const { getByTestId, unmount } = render(
+    <DocumentPortal>
+      <dialog data-testid="dialog">Hello!</dialog>
+    </DocumentPortal>
+  );
   const targetElement = getByTestId('dialog');
   unmount();
-  expect(parentElement).not.toBeInTheDocument();
   expect(targetElement).not.toBeInTheDocument();
+});
+
+test('uses `as` prop for portal container tagName', () => {
+  const { getByTestId, rerender } = render(
+    <DocumentPortal as="aside">
+      <dialog data-testid="dialog">Hello!</dialog>
+    </DocumentPortal>
+  );
+  expect(getByTestId('dialog').parentNode.tagName).toBe('ASIDE');
+  rerender(
+    <DocumentPortal as="span">
+      <dialog data-testid="dialog">Hello!</dialog>
+    </DocumentPortal>
+  );
+  expect(getByTestId('dialog').parentNode.tagName).toBe('SPAN');
 });
 
 test('updates function refs', () => {
