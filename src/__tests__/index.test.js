110461b92218bc92b500c48df0c7cdf5c47eee34
Jacob Buck <buck.jacob@gmail.com>
2021-12-15 08:14:34 +0000

    Add test coverage for hydration from ssr

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,7 +3,14 @@
  */
 import { render, screen, waitFor } from '@testing-library/react';
 import { createRef } from 'react';
+import { renderToString } from 'react-dom/server';
 import DocumentPortal from '..';
+import useBrowserLayoutEffect from '../useBrowserLayoutEffect';
+
+jest.mock('../useBrowserLayoutEffect', () => ({
+  __esModule: true,
+  default: jest.fn(jest.requireActual('../useBrowserLayoutEffect').default),
+}));
 
 test('renders child inside portal', async () => {
   render(
@@ -107,4 +114,27 @@ test('handles changed ref', async () => {
   expect(ref1).toHaveBeenLastCalledWith(null);
   const dialogElement = await screen.findByTestId('dialog');
   expect(ref2.current).toBe(dialogElement.parentNode);
+});
+
+test('hydrates from server-side rendering', async () => {
+  const TestComponent = () => (
+    <main data-testid="main">
+      <DocumentPortal>
+        <dialog data-testid="dialog">Hello!</dialog>
+      </DocumentPortal>
+    </main>
+  );
+
+  useBrowserLayoutEffect.mockImplementationOnce(() => {});
+  useBrowserLayoutEffect.mockImplementationOnce(() => {});
+  useBrowserLayoutEffect.mockImplementationOnce(() => {});
+
+  const container = document.createElement('div');
+  container.innerHTML = renderToString(<TestComponent />);
+  document.body.appendChild(container);
+
+  render(<TestComponent />, { container, hydrate: true });
+
+  const dialogElement = await screen.findByTestId('dialog');
+  expect(dialogElement).toBeInTheDocument();
 });
